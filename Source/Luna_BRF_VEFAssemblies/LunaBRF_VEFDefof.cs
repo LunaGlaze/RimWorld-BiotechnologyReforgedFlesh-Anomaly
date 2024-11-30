621032e3c8912edbbb776b30b3bbdd1d67ebce83
LunaGlaze <shiqi.luna@outlook.com>
2024-11-30 20:49:29 +0800

    Balancing and trying to fix DigesterIPipe

---
+++ Source/Luna_BRF_VEFAssemblies/LunaBRF_VEFDefof.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Luna_BRF_VEFAssemblies
+{
+    [DefOf]
+    public static class LunaBRF_VEFDefof
+    {
+        public static JobDef BRF_RefuelDigesterIPipe;
+
+        public static JobDef BRF_RefuelDigesterIPipe_Atomic;
+    }
+}
---
+++ Source/Luna_BRF_VEFAssemblies/WorkGiver_RefuelDigesterIPipe.cs
@@ -0,0 +1,36 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace Luna_BRF_VEFAssemblies
+{
+	public class WorkGiver_RefuelDigesterIPipe : WorkGiver_Scanner
+	{
+		public override ThingRequest PotentialWorkThingRequest => ThingRequest.ForGroup(ThingRequestGroup.Refuelable);
+
+		public override PathEndMode PathEndMode => PathEndMode.Touch;
+
+		public virtual JobDef JobStandard => LunaBRF_VEFDefof.BRF_RefuelDigesterIPipe;
+
+		public virtual JobDef JobAtomic => LunaBRF_VEFDefof.BRF_RefuelDigesterIPipe_Atomic;
+
+		public virtual bool CanRefuelThing(Thing t)
+		{
+			return !(t is Building_Turret);
+		}
+
+		public override bool HasJobOnThing(Pawn pawn, Thing t, bool forced = false)
+		{
+			if (CanRefuelThing(t))
+			{
+				return RefuelWorkGiverUtilityDigesterIPipe.CanRefuel(pawn, t, forced);
+			}
+			return false;
+		}
+
+		public override Job JobOnThing(Pawn pawn, Thing t, bool forced = false)
+		{
+			return RefuelWorkGiverUtilityDigesterIPipe.RefuelJob(pawn, t, forced, JobStandard, JobAtomic);
+		}
+	}
+}
